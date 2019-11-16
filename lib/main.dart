70f74ab782ff1ba9b65f64b72d128d3244c71fd9
Diego Spranger <daspranger@hotmail.com>
2019-11-16 13:09:37 +0100

    Basic UI implemented

--- lib/main.dart
+++ lib/main.dart
@@ -24,4 +24,60 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {}
+class _MyHomePageState extends State<MyHomePage> {
+  Widget buildButton(String buttonText) {
+    return new Expanded(
+      child: new OutlineButton(
+        padding: new EdgeInsets.all(24.0),
+        child: new Text(buttonText),
+        onPressed: () => {},
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: new Container(
+          child: new Column(children: <Widget>[
+            new Container(child: new Text("Result")),
+            new Expanded(
+              child: new Divider(),
+            ),
+            new Column(children: [
+              new Row(children: [
+                buildButton("7"),
+                buildButton("8"),
+                buildButton("9"),
+                buildButton("/"),
+              ]),
+              new Row(children: [
+                buildButton("4"),
+                buildButton("5"),
+                buildButton("6"),
+                buildButton("X"),
+              ]),
+              new Row(children: [
+                buildButton("1"),
+                buildButton("2"),
+                buildButton("3"),
+                buildButton("-"),
+              ]),
+              new Row(children: [
+                buildButton("."),
+                buildButton("0"),
+                buildButton("00"),
+                buildButton("+"),
+              ]),
+              new Row(children: [
+                buildButton("CLEAR"),
+                buildButton("="),
+              ]),
+            ])
+          ]),
+        ));
+  }
+}
